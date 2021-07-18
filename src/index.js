3cd7d68b1934cb2f1f384096a0d0c1dbf8e894f1
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2021-07-18 13:34:20 -0400

    Add More Methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
+  //Player Retrieving Methods
   getQueue: require('./methods/getQueue'),
+  getPlaying: require('./methods/getPlaying'),
+  getLyrics: require('./methods/getLyrics'),
+  
+  //Player Transmitting Methods
+  setVolmue: require('./methods/setVolume'),
+  setLoop: require('./methods/setLoop'),
+  playTrack: require('./methods/playTrack'),
+  skipTrack: require('./methods/skipTrack'),
+  resumeTrack: require('./methods/resumeTrack'),
+  pauseTrack: require('./methods/pauseTrack')
 }
